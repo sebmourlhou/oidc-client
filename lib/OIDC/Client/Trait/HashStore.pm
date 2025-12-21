8dfd27af5ee4441e217a945f48835124f8d3c3ef
sebmourlhou <sebmourlhou@yahoo.fr>
2025-12-21 00:06:58 +0000

    Support for after_touching_session callback (needed for Dancer2)

---
+++ lib/OIDC/Client/Trait/HashStore.pm
@@ -0,0 +1,18 @@
+package OIDC::Client::Trait::HashStore;
+use utf8;
+use Moose::Role;
+use namespace::autoclean;
+use OIDC::Client::Utils qw(reach_data affect_data delete_data);
+
+# creates read_session(), write_session() and delete_session() for 'session' attribute
+# creates read_stash(), write_stash() and delete_stash() for 'stash' attribute
+after 'install_accessors' => sub {
+  my $self = shift;
+  my $realclass = $self->associated_class();
+  my $name = $self->name;
+  $realclass->add_method("read_${name}"   => sub { return reach_data( $_[0]->$name, $_[1]) });
+  $realclass->add_method("write_${name}"  => sub { return affect_data($_[0]->$name, $_[1], $_[2]) });
+  $realclass->add_method("delete_${name}" => sub { return delete_data ($_[0]->$name, $_[1]) });
+};
+
+1;
